d804b41f5ceaafb2e66983576c4dcfcc9c2b837f
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2026-07-26 09:02:38 +1000

    Initial commit

---
+++ inc/namespace.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WP Plugin Template
+ *
+ * @package           WpPluginTemplate
+ */
+
+namespace PWCC\WpPluginTemplate;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+const PLUGIN_VERSION = '1.0.0';
+
+/**
+ * Bootstrap the plugin.
+ */
+function bootstrap() {
+}
